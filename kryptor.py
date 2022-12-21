3b13fd10a582e672f4f3293bb0e638339fa0cc11
Péricles Borges <92488227+KaBuLoZu@users.noreply.github.com>
2022-12-21 18:41:37 -0100

    Add files via upload

---
+++ kryptor.py
@@ -0,0 +1,62 @@
+import os
+from cryptography.fernet import Fernet
+import pyfiglet
+
+banner = pyfiglet.figlet_format("KRYPTOR")
+print(banner)
+print("                            @_KaBuLoZu_\n\n")
+print("  Select: \n")
+print("----------------------------------------\n")
+print("	Encrypt Files ---------->[1]\n")
+print("	Decrypt Files ---------->[2]\n")
+print("----------------------------------------")
+
+x = int(input("\n	--> "))
+
+if x == 1:
+	
+	files = []
+
+	for file in os.listdir():
+		if file == "kryptor.py" or file == "thekey.key":
+		        continue
+		if os.path.isfile(file):
+		        files.append(file)
+		        
+	key = Fernet.generate_key()
+	
+
+	with open("thekey.key", "wb") as thekey:
+		thekey.write(key)
+
+	for file in files:
+		with open(file, "rb") as thefile:
+		        contents = thefile.read()
+		contents_encrypted = Fernet(key).encrypt(contents)
+		with open(file, "wb") as thefile:
+		        thefile.write(contents_encrypted)
+	print("\n  Done")
+
+if x == 2:
+	files = []
+
+	for file in os.listdir():
+		if file == "kryptor.py" or file == "thekey.key":
+		        continue
+		if os.path.isfile(file):
+		        files.append(file)
+
+	with open("thekey.key", "rb") as key:
+		secretkey = key.read() 
+		
+	for file in files:
+			with open(file, "rb") as thefile:
+				contents = thefile.read()
+			contents_decrypted = Fernet(secretkey).decrypt(contents)
+			with open(file, "wb") as thefile:
+				thefile.write(contents_decrypted)
+	print("\n  Done")
+
+		
+
+
